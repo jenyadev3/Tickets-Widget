3d90425051342906814b959d5a81b9651cf95855
Evgeniya <112981213+jenyadev3@users.noreply.github.com>
2023-05-16 22:34:19 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,47 @@
+const tickets = [
+    {
+      date: "2023-03-05",
+      economy: 12000,
+      business: 34000
+    },
+    {
+      date: "2023-03-01",
+      economy: 11000,
+      business: 37000
+    },
+    {
+      date: "2023-03-03",
+      economy: 14000,
+      business: 32000
+    },
+    {
+      date: "2023-03-09",
+      economy: 12000,
+      business: 34000
+    },
+    {
+      date: "2023-03-08",
+      economy: 11000,
+      business: 37000
+    },
+    {
+      date: "2023-03-06",
+      economy: 14000,
+      business: 32000
+    },
+    {
+      date: "2023-03-02",
+      economy: 12000,
+      business: 34000
+    },
+    {
+      date: "2023-03-04",
+      economy: 11000,
+      business: 37000
+    },
+    {
+      date: "2023-03-07",
+      economy: 14000,
+      business: 32000
+    }
+  ];
